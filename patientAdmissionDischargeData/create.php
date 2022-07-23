10044bc81523d93d03f161b84cbcd0b69f8e165f
JawadBelloA <jawadcodemail@gmail.com>
2022-07-23 15:23:13 +0100

    update title

--- patientAdmissionDischargeData/create.php
+++ patientAdmissionDischargeData/create.php
@@ -24,7 +24,7 @@ if(isset($_POST['submit'])){
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UDUTHSOKOTO-Patient Personal Data</title>
+    <title>UDUTH SOKOTO - Patient Personal Data</title>
     <link rel="stylesheet" href="/css/bulma.min.css">
 </head>
 <body>
